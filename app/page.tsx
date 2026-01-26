7155d01e73886e0429362d2489ae21f533b108a5
Tiago Silvino <tssdj13@gmail.com>
2026-01-26 13:06:56 -0300

    feat: add BarbershopItem component

--- app/page.tsx
+++ app/page.tsx
@@ -3,15 +3,24 @@ import Header from "./_components/header"
 import { Button } from "./_components/ui/button"
 import { Input } from "./_components/ui/input"
 import Image from "next/image"
+import { Card, CardContent } from "./_components/ui/card"
+import { Badge } from "./_components/ui/badge"
+import { Avatar, AvatarImage } from "./_components/ui/avatar"
+import { db } from "./_lib/prisma"
+import BarbershopItem from "./_components/barbershop-item"
 
-export default function Home() {
+const Home = async () => {
+  const barbershops = await db.barbershop.findMany({})
+
   return (
     <div>
       <Header />
       <div className="p-5">
+        {/* TEXTO */}
         <h2 className="text-xl font-semibold">Olá, Teste</h2>
         <p>Segunda-Feira, 26 de Janeiro</p>
 
+        {/* BUSCA */}
         <div className="mt-6 flex items-center gap-2">
           <Input placeholder="Faça sua busca..." />
           <Button>
@@ -19,6 +28,7 @@ export default function Home() {
           </Button>
         </div>
 
+        {/* BANNER */}
         <div className="relative mt-6 h-[150px] w-full">
           <Image
             alt="Agende nos melhores com FSW Barber"
@@ -27,7 +37,46 @@ export default function Home() {
             className="rounded-xl object-cover"
           />
         </div>
+
+        {/* AGENDAMENTO */}
+        <h2 className="mb-3 mt-6 text-xs font-bold uppercase text-gray-400">
+          Agendamentos
+        </h2>
+        <Card>
+          <CardContent className="flex justify-between p-0">
+            {/* ESQUERDA */}
+            <div className="flex flex-col gap-2 py-5 pl-5">
+              <Badge className="w-fit">Confirmado</Badge>
+              <h3 className="font-semibold">Corte de Cabelo</h3>
+
+              <div className="flex items-center">
+                <Avatar className="mr-2 h-6 w-6">
+                  <AvatarImage src="https://utfs.io/f/5832df58-cfd7-4b3f-b102-42b7e150ced2-16r.png" />
+                </Avatar>
+                <p className="text-sm">Barbearia FSW barber</p>
+              </div>
+            </div>
+
+            {/* DIREITA */}
+            <div className="flex flex-col items-center justify-center border-l-2 border-solid px-5">
+              <p className="text sm">Agosto</p>
+              <p className="text-2xl">05</p>
+              <p className="text-sm">10:59</p>
+            </div>
+          </CardContent>
+        </Card>
+
+        <h2 className="mb-3 mt-6 text-xs font-bold uppercase text-gray-400">
+          Recomendados
+        </h2>
+        <div className="flex gap-4 overflow-auto [&::-webkit-scrollbar]:hidden">
+          {barbershops.map((barbershop) => (
+            <BarbershopItem key={barbershop.id} barbershop={barbershop} />
+          ))}
+        </div>
       </div>
     </div>
   )
 }
+
+export default Home
