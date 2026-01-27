30cf7f3de95766c31862ed100e29d3998b9ec63a
Tiago Silvino <tssdj13@gmail.com>
2026-01-27 15:47:50 -0300

    chore: add next auth setup

--- app/_components/sidebar-sheet.tsx
+++ app/_components/sidebar-sheet.tsx
@@ -3,13 +3,21 @@ import {
   SheetHeader,
   SheetTitle,
   SheetClose,
-} from "@/components/ui/sheet"
-import { Avatar, AvatarImage } from "./ui/avatar"
-import { HomeIcon, CalendarIcon, LogOutIcon } from "lucide-react"
+} from "@/app/_components/ui/sheet"
+// import { Avatar, AvatarImage } from "./ui/avatar"
+import { HomeIcon, CalendarIcon, LogOutIcon, LogInIcon } from "lucide-react"
 import { quickSearchOption } from "../_constants/search"
 import { Button } from "./ui/button"
 import Link from "next/link"
 import Image from "next/image"
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "./ui/dialog"
 
 const Sidebar = () => {
   return (
@@ -18,15 +26,41 @@ const Sidebar = () => {
         <SheetTitle className="text-left">Menu</SheetTitle>
       </SheetHeader>
 
-      <div className="flex items-center gap-3 border-b border-solid py-5">
-        <Avatar>
-          <AvatarImage src="https://images.unsplash.com/photo-1534528741775-53994a69daeb?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTR8fHBlc3NvYXxlbnwwfHwwfHx8MA%3D%3D" />
-        </Avatar>
+      <div className="flex items-center justify-between gap-3 border-b border-solid py-5">
+        <h2 className="font-bold">Olá, faça seu login!</h2>
+        <Dialog>
+          <DialogTrigger asChild>
+            <Button size="icon">
+              <LogInIcon />
+            </Button>
+          </DialogTrigger>
+          <DialogContent className="w-[90%]">
+            <DialogHeader>
+              <DialogTitle> Faça seu login na plataforma! </DialogTitle>
+              <DialogDescription>
+                Conecte-se usando sua conta do Google!
+              </DialogDescription>
+            </DialogHeader>
 
-        <div>
+            <Button variant="outline" className="gap-1 font-bold">
+              <Image
+                src="/google.svg"
+                alt="Fazer login com o google"
+                width={18}
+                height={18}
+              />
+              Google
+            </Button>
+          </DialogContent>
+        </Dialog>
+        {/* <Avatar>
+          <AvatarImage src="https://images.unsplash.com/photo-1534528741775-53994a69daeb?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTR8fHBlc3NvYXxlbnwwfHwwfHx8MA%3D%3D" />
+        </Avatar> */}
+
+        {/* <div>
           <p className="font-bold">Amanda Rocha</p>
           <p className="text-xs">amandarocha@fullstack.io</p>
-        </div>
+        </div> */}
       </div>
 
       <div className="flex flex-col gap-1 border-b border-solid py-5">
